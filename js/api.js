96d857ccf3e016949e10c9329e4e09c982f76dee
Jack Mandelkorn <jack@jackmandelkorn.com>
2022-02-21 18:59:34 -0800

    updating internal site with coming soon

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const API_KEY = "pwk_702297ce-39a0-4738-b9f8-7166e6c0f93f"
 
 const DATA_API = {
-  getOutstandingBalances: (callback = (r) => { console.log(r) }) => {
+  getOutstandingBalances: (callback = (r) => {}) => {
     $.ajax({
       url: (API + "/api/scan/all"),
       type: "PUT",
@@ -19,5 +19,73 @@ const DATA_API = {
         callback(sum / 100)
       }
     })
-  }
+  },
+  getAllFeedback: (callback = (r) => {}) => {
+    $.ajax({
+      url: (API + "/api/scan/all"),
+      type: "PUT",
+      data: {
+        params: JSON.stringify({
+          TableName: "feedback"
+        }),
+        key: API_KEY,
+      },
+      success: (data) => {
+        let sum = 0
+        let c = 0
+        for (let point of data) {
+          const factor = parseInt(point.factor.N)
+          if (factor === (-2)) {
+            sum += 0.59 //F
+          }
+          if (factor === (-1)) {
+            sum += ((0.7 + 0.73) / 2) //C-
+          }
+          else if (factor === 1) {
+            sum += ((0.87 + 0.9) / 2) //B+
+          }
+          else if (factor === 2) {
+            sum += ((0.97 + 1) / 2) //A+
+          }
+          c++
+        }
+        callback(sum / c)
+      }
+    })
+  },
+  getFeedbackOnDay: (day, callback = (r) => {}) => {
+    $.ajax({
+      url: (API + "/api/scan/all"),
+      type: "PUT",
+      data: {
+        params: JSON.stringify({
+          TableName: "feedback"
+        }),
+        key: API_KEY,
+      },
+      success: (data) => {
+        let sum = 0
+        let c = 0
+        for (let point of data) {
+          if (parseInt(point.id.S.split("-").reverse()[0]) === day) {
+            const factor = parseInt(point.factor.N)
+            if (factor === (-2)) {
+              sum += 0.59 //F
+            }
+            if (factor === (-1)) {
+              sum += ((0.7 + 0.73) / 2) //C-
+            }
+            else if (factor === 1) {
+              sum += ((0.87 + 0.9) / 2) //B+
+            }
+            else if (factor === 2) {
+              sum += ((0.97 + 1) / 2) //A+
+            }
+            c++
+          }
+        }
+        callback(sum / c)
+      }
+    })
+  },
 }
