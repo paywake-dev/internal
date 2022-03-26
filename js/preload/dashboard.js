e7be6ede0b0f33deba5139195d007b048ddcc345
Jack Mandelkorn <jack@jackmandelkorn.com>
2022-03-25 20:24:04 -0700

    Update dashboard.js

--- js/preload/dashboard.js
+++ js/preload/dashboard.js
@@ -132,26 +132,32 @@ const fillAttributes = () => {
       ROUTINES.logout()
       return;
     }
-    let name = UNFILLED
-    let title = UNFILLED
-    for (let attribute of data) {
-      if (attribute.Name === "name") {
-        name = attribute.Value
-      }
-      else if (attribute.Name === "custom:title") {
-        title = attribute.Value
-      }
-    }
-    for (let element of (document.getElementsByClassName("__full-name-fill") || [])) {
-      element.innerHTML = name
-    }
-    for (let element of (document.getElementsByClassName("__first-name-fill") || [])) {
-      element.innerHTML = name.split(" ")[0].trim()
-    }
-    for (let element of (document.getElementsByClassName("__title-fill") || [])) {
-      element.innerHTML = title
-    }
+    localStorage.setItem(LOCAL_STORAGE_TAG + "name-data", JSON.stringify(data))
+    setNameData()
   })
+}
+
+const setNameData = () => {
+  const data = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TAG + "name-data") || "[]")
+  let name = UNFILLED
+  let title = UNFILLED
+  for (let attribute of data) {
+    if (attribute.Name === "name") {
+      name = attribute.Value
+    }
+    else if (attribute.Name === "custom:title") {
+      title = attribute.Value
+    }
+  }
+  for (let element of (document.getElementsByClassName("__full-name-fill") || [])) {
+    element.innerHTML = name
+  }
+  for (let element of (document.getElementsByClassName("__first-name-fill") || [])) {
+    element.innerHTML = name.split(" ")[0].trim()
+  }
+  for (let element of (document.getElementsByClassName("__title-fill") || [])) {
+    element.innerHTML = title
+  }
 }
 
 
